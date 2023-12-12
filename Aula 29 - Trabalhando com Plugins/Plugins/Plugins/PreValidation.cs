4c153b4aad7a04bcbcad3e5b411857953ff620aa
Edinaldo-C-Silva-Jr <edinaldo.csilvajr@outlook.com>
2023-12-12 20:25:01 -0300

    Cria um projeto de Plugin na Aula 29. Cria um plugin de Pre-Registration.

---
+++ Aula 29 - Trabalhando com Plugins/Plugins/Plugins/PreValidation.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+
+namespace Plugins
+{
+    public class PreValidation : IPlugin
+    {
+        public void Execute(IServiceProvider serviceProvider)
+        {
+            IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            IOrganizationServiceFactory serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+            IOrganizationService service = serviceFactory.CreateOrganizationService(null);
+            ITracingService tracer = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+
+            if (context.InputParameters.Contains("Target"))
+            {
+                Entity contextEntity = (Entity)context.InputParameters["Target"];
+                tracer.Trace($"Entidade do Contexto: {contextEntity.Attributes.Count}");
+
+                if (contextEntity == null)
+                {
+                    return;
+                }
+
+                if (!contextEntity.Contains("telephone1"))
+                {
+                    throw new InvalidPluginExecutionException("Campo de Telefone Principal é obrigatório!");
+                }
+            }
+        }
+    }
+}
