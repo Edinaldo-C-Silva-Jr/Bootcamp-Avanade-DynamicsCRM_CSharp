22d2b08da843aad63d6c1abc9497d1edf44ccef1
Edinaldo-C-Silva-Jr <edinaldo.csilvajr@outlook.com>
2023-12-13 19:35:24 -0300

    Cria o primeiro projeto de Custom Actions, com um script em Javascript para chamar a action, e o código da action em si.

---
+++ Aula 30 - Trabalhando com Actions/CustomAction/CustomAction/FirstCustomAction.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Xrm.Sdk;
+using System;
+
+namespace CustomAction
+{
+    public class FirstCustomAction : IPlugin
+    {
+        public void Execute (IServiceProvider serviceProvider)
+        {
+            IPluginExecutionContext context = (IPluginExecutionContext)serviceProvider.GetService(typeof(IPluginExecutionContext));
+            IOrganizationServiceFactory serviceFactory = (IOrganizationServiceFactory)serviceProvider.GetService(typeof(IOrganizationServiceFactory));
+            IOrganizationService service = serviceFactory.CreateOrganizationService(null);
+            ITracingService tracer = (ITracingService)serviceProvider.GetService(typeof(ITracingService));
+
+            tracer.Trace("A Primeira Action foi executada com sucesso!");
+        }
+    }
+}
